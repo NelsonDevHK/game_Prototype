13ab7ee3dcedcfd2cd84dce93a310a858f6b47bf
NelsonDevHK <songkuenhk@gmail.com>
2020-09-16 12:32:52 +0800

    clear screen and hide exit

--- GameLogic.java
+++ GameLogic.java
@@ -3,22 +3,44 @@ import java.util.concurrent.TimeUnit;
 
 class GameWorld {
     private int MAX_H = 10;
-    private int MAX_W = 10;
+    private int MAX_W = 20;
     Scanner sc;
     String[][] gmap;
     int posx = 0;
     int posy = 0;
 
-    int winX = 9;
-    int winY = 9;
+    int winX = 14;
+    int winY = 5;
 
-    boolean ifWin() {
+    boolean Win() {
         return (posx == winX && posy == winY);
     }
 
+    void useitem() {
+            for (int i = 0; i < MAX_H; i++) {
+                for (int j = 0; j < MAX_W; j++) {
+                    if (i == posy && j == posx)
+                        System.out.print("N");
+                    else if (i == winY && j == winX) {
+                        System.out.print("O");
+
+                    } else
+                        System.out.print(gmap[i][j]);
+                }
+                System.out.println();
+            }
+        sleep1sec();
+    }
+
+    void intsruction() {
+        System.out.println("A = move left");
+        System.out.println("D = move right");
+        System.out.println("W = move up");
+        System.out.println("S = move down");
+        System.out.println("F = use item(show the Winnig Exit)");
+    }
+
     void move() {
-        System.out.println("choose move direction");
-        System.out.println(posx + "   " + posy);
         String choice = sc.nextLine();
         switch (choice) {
             case "A":
@@ -33,6 +55,8 @@ class GameWorld {
             case "D":
                 this.posx += 1;
                 break;
+            case "F":
+                useitem();
         }
         if (posx < 0)
             this.posx = 0;
@@ -42,8 +66,11 @@ class GameWorld {
             this.posy = 0;
         if (posy >= MAX_H)
             this.posy = MAX_H - 1;
+
+        clearScreen();
     }
 
+    // Constructer
     GameWorld() {
         sc = new Scanner(System.in);
         gmap = new String[MAX_H][MAX_W];
@@ -54,13 +81,12 @@ class GameWorld {
         }
     }
 
+    // vander update the graphic only no logic!!!!!!
     void vander() {
         for (int i = 0; i < MAX_H; i++) {
             for (int j = 0; j < MAX_W; j++) {
                 if (i == posy && j == posx)
                     System.out.print("N");
-                else if (i == winY && j == winX)
-                    System.out.print("O");
                 else
                     System.out.print(gmap[i][j]);
             }
@@ -68,12 +94,15 @@ class GameWorld {
         }
     }
 
+    // main loop of this game
     void gameLoop() {
+        intsruction();
+
         while (true) {
-            move();
-            if (ifWin())
-                break;
             vander();
+            move();
+            if (Win())
+                break;
         }
         System.out.println("WINNER!");
     }
@@ -84,6 +113,11 @@ class GameWorld {
         } catch (InterruptedException ie) {
 
         }
+    }
+
+    void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
 
